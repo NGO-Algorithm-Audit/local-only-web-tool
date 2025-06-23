45f1d40834ccdc1b211eb424f9c74d970d9998e9
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-06-23 17:54:33 +0200

    TODO"s added for bias variable

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -45,10 +45,10 @@ export const en = {
                     dataSetTooltip: `Preprocess your data such that: 
                     - missing values are removed or replaced;
                     - all columns (except your bias variable column) should have the same datatypes, e.g., numerical or categorical;
-                    - the bias variable column is numerical`,
+                    - the bias variable column is categorical`,
                     performanceMetric: 'Bias variable',
                     performanceMetricTooltip:
-                        'Clustering will be performed on the bias variable. The bias variable should be numerical. Examples of bias variables are "being classified as high risk" or "selected for an investigation"',
+                        'Clustering will be performed on the bias variable. The bias variable should be categorical. Examples of bias variables are "being classified as high risk" or "selected for an investigation"',
                     dataType: 'Type of data',
                     dataTypeTooltip:
                         'Specify whether the data are categorical or numerical. All columns (except your bias variable column) should have the same data type',
@@ -289,6 +289,7 @@ missing data are imputed. For {tooltip:syntheticData.missingDataMCARTooltip}Miss
     },
 
     biasAnalysis: {
+        // TODO 295,296 : bias variable vervangen door gekozen bias variable
         testingStatisticalSignificance: `**5. Testing cluster differences wrt. bias variable**
 
 - <i class="font-serif">H</i><sub>0</sub>: no difference in bias variable between the most deviating cluster and the rest of the dataset
@@ -395,6 +396,7 @@ In this example, we analyze which group is most adversely affected by the risk p
             valueText:
                 'Number of datapoints in cluster {{index}}: {{value}} / {{totalRecords}}',
         },
+        // TODO : 400,401 : bias variable vervangen door gekozen bias variable
         higherAverage: `The most deviating cluster has statistically significant different bias variable than the rest of the dataset.`,
         noSignificance: `No statistically significant difference in bias variable between the most biased cluster and the rest of the dataset.`,
 
